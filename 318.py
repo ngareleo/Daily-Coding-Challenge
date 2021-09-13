01f4b05c40e83e57890d837a083234082e0dc67e
Leo <ngareoel@gmail.com>
2021-09-13 17:38:19 +0300

    day 318 work

---
+++ 318.py
@@ -0,0 +1,18 @@
+"""
+This problem was asked by Apple.
+
+You are going on a road trip, and would like to create a suitable music playlist.
+The trip will require N songs, though you only have M songs downloaded, where M < N.
+A valid playlist should select each song at least once, and guarantee a buffer of B songs between repeats.
+
+Given N, M, and B, determine the number of valid playlists.
+"""
+
+
+def main(n, m, b):
+    if m > n:
+        return -1
+
+
+if __name__ == '__main__':
+    pass
